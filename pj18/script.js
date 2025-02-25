eef3e143ea9484357f0f74381ec9ebbea76c2dd7
teddygraham <53947819+teddygraham@users.noreply.github.com>
2025-02-25 11:15:45 -0600

    Breaking each of the functions into separate files

---
+++ pj18/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const textInput = document.getElementById("textInput");
+    const charCount = document.getElementById("charCount");
+
+    textInput.addEventListener("input", () => {
+        charCount.textContent = textInput.value.length;
+    });
+});
+
